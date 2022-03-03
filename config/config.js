8fc8ac97a15af03f407b7afa8079f2a2b5850aeb
breaze <simon.gaviriao@gmail.com>
2022-03-03 13:34:29 -0500

    Host configured to work in heroku

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 //Import firebase admin
 let admin = require("firebase-admin");
-const PORT = process.env.port || 3800;
-const SERVER = "127.0.0.1";
+const PORT = process.env.PORT || 3800;
+const SERVER = "0.0.0.0";
 //Import firestore key
 let serviceAccount = require("./secret_key.json");
 admin.initializeApp({
